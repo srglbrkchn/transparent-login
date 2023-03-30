cf936f736c2874ac05eecc5ad38aa516accfb50e
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2023-03-30 20:05:19 +0200

    finish register route.

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
+
+const users = [];
 
 app.use(express.static(__dirname + "/public"));
 
@@ -16,13 +19,29 @@ app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
 
+// login post route
+app.post("/login", (req, res) => {});
+
 // register get route
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
 // register post route
-app.post("/register", (req, res) => {});
+app.post("/register", async (req, res) => {
+  try {
+    const hashPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashPassword,
+    });
+    res.redirect("/login");
+  } catch (e) {
+    res.redirect("/register");
+  }
+});
 
 app.listen(3000, (req, res) => {
   console.log("The server is up and running!");
