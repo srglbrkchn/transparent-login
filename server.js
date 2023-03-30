8a26cacc9b90ce1912c7139e08480708a269af53
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2023-03-30 20:54:29 +0200

    create local passport file for authenticating user by username and password.

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
+const passport = require("passport");
+
+const initializePassport = require("./passport-config ");
+initializePassport(passport);
 
 const users = [];
 
 app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
 
 // set up ejs as a view engine
 app.set("view-engine", "ejs");
@@ -39,8 +44,10 @@ app.post("/register", async (req, res) => {
     });
     res.redirect("/login");
   } catch (e) {
+    console.log(e);
     res.redirect("/register");
   }
+  console.log(users);
 });
 
 app.listen(3000, (req, res) => {
